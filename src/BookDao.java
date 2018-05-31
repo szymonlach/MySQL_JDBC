586c829adeaffe5e7cd49a51c8afca1b72e44341
Szymon Lach <szymom.sp1@gmail.com>
2018-05-31 12:20:39 +0200

    initialize

--- src/BookDao.java
+++ src/BookDao.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class BookDao {
+
+    private static final String url = "jdbc:mysql://localhost:3306/library";
+    private static final String username = "root";
+    private static final String password = "Student140";
+
+    private Connection connection;
+
+
+    public BookDao() {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/ConnectMySQL.java
+++ src/ConnectMySQL.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import com.mysql.jdbc.Connection;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectMySQL {
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/library";
+        String username = "root";
+        String password = "Student140";
+        Connection connection = (Connection) DriverManager.getConnection(url, username, password);
+
+
+        Statement statement = connection.createStatement();
+        String query = "select * from books";
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            int id = resultSet.getInt(1);
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            String year = resultSet.getString("year");
+            String isbn = resultSet.getString("isbn");
+            System.out.println(id + " " + title + " " + author + " " + year + " " + isbn);
+
+        }
+
+
+        connection.close();
+    }
+
+
 }
